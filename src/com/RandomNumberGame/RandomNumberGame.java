01d6dd8eacac0b04b3f052eaef790adf1779c601
John Detter <john@detter.com>
2014-11-20 15:28:49 -0600

    Changed some formatting problems

--- src/com/RandomNumberGame/RandomNumberGame.java
+++ src/com/RandomNumberGame/RandomNumberGame.java
@@ -1,9 +1,9 @@
 package com.RandomNumberGame;
+
 import java.util.*;
 
-
-public class RandomNumberGame {
-
+public class RandomNumberGame 
+{
 	public static void main(String[] args) 
 	{
 		Scanner playerGuess = new Scanner(System.in);
@@ -11,38 +11,39 @@ public class RandomNumberGame {
 		int attempts = 0;
 		while(playing)
 		{
-		int guess;
-		Random generator = new Random();
-		int value = generator.nextInt(100);
+			int guess;
+			Random generator = new Random();
+			int value = generator.nextInt(100);
+
+			while(attempts<2)
+			{
+				System.out.println("Guess: ");
+				guess = playerGuess.nextInt();
+				if(guess == value)
+				{
+					System.out.println("Congrats! You guessed correct!");
+					playing=false;
+				}else if(guess>value){
+					System.out.println("The value is lower than your guess. Please try again.");
+
+				}else if(guess<value){
+					System.out.println("The value is higher than your guess. Please try again");
+				}
+				attempts++;
+			}
+			System.out.println("Final Guess: ");
+			guess = playerGuess.nextInt();
+			if(guess==value)
+			{
+				System.out.println("Congrats! You Guessed Correct! The value was: " + value);
+				playing= false;
+			}else if(guess!=value){
+				System.out.println("The correct value was: " + value);
+				playing=  false;
+			}
+
+		}
 		
-		while(attempts<2)
-		{
-		System.out.println("Guess: ");
-		guess = playerGuess.nextInt();
-		if(guess == value)
-		{
-			System.out.println("Congrats! You guessed correct!");
-			playing=false;
-		}else if(guess>value){
-			System.out.println("The value is lower than your guess. Please try again.");
-			
-		}else if(guess<value){
-			System.out.println("The value is higher than your guess. Please try again");
-		}
-		attempts++;
-		}
-		System.out.println("Final Guess: ");
-		guess = playerGuess.nextInt();
-		if(guess==value)
-		{
-		System.out.println("Congrats! You Guessed Correct! The value was: " + value);
-		playing= false;
-		}else if(guess!=value){
-			System.out.println("The correct value was: " + value);
-			playing=  false;
-		}
-
+		playerGuess.close();
 	}
-	}
-
 }
